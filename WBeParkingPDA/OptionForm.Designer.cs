6d9261103322e90c0de5951dfee90f12f42c1e91
ediux <ediux@msn.com>
2016-11-20 22:55:45 +0800

    加入設定功能頁

---
+++ WBeParkingPDA/OptionForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WBeParkingPDA
+{
+    partial class OptionForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改這個方法的內容。
+        ///
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblServerIP = new System.Windows.Forms.Label();
+            this.tbServerIP = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblServerIP
+            // 
+            this.lblServerIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblServerIP.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Regular);
+            this.lblServerIP.Location = new System.Drawing.Point(3, 49);
+            this.lblServerIP.Name = "lblServerIP";
+            this.lblServerIP.Size = new System.Drawing.Size(234, 39);
+            this.lblServerIP.Text = "同步伺服器IP:";
+            // 
+            // tbServerIP
+            // 
+            this.tbServerIP.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbServerIP.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Regular);
+            this.tbServerIP.Location = new System.Drawing.Point(3, 91);
+            this.tbServerIP.Name = "tbServerIP";
+            this.tbServerIP.Size = new System.Drawing.Size(234, 42);
+            this.tbServerIP.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSave.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Bold);
+            this.btnSave.Location = new System.Drawing.Point(3, 319);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(93, 40);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "儲存";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Font = new System.Drawing.Font("Tahoma", 22F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.Location = new System.Drawing.Point(144, 319);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(93, 40);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // OptionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(240, 400);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblServerIP);
+            this.Controls.Add(this.tbServerIP);
+            this.Controls.Add(this.btnSave);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "OptionForm";
+            this.Text = "設定";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.OptionForm_Load);
+            this.Controls.SetChildIndex(this.btnSave, 0);
+            this.Controls.SetChildIndex(this.tbServerIP, 0);
+            this.Controls.SetChildIndex(this.lblServerIP, 0);
+            this.Controls.SetChildIndex(this.btnCancel, 0);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblServerIP;
+        private System.Windows.Forms.TextBox tbServerIP;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
